db4e3ca0e9f8d6d41a1baec5a77af8025021c53a
junho <qkrwnsgh1288@daum.net>
2023-05-20 23:53:16 +0900

    impl getauth && callback

--- index.js
+++ index.js
@@ -65,12 +65,41 @@ async function GetAuthRequest(req,res) {
     const scope = request.body.scope ?? [];
     request.body.scope = [...scope, proofRequest];
     console.log(request);
-    console.log(scope);
+    console.log(request.body.scope);
 
     // Store auth request in map associated with session ID
     requestMap.set(`${sessionId}`, request);
+    const aaa = {
+        "id": "5a044958-93ee-4d55-b3a7-4e01293c7792",
+        "typ": "application/iden3comm-plain-json",
+        "type": "https://iden3-communication.io/authorization/1.0/request",
+        "thid": "5a044958-93ee-4d55-b3a7-4e01293c7792",
+        "body": {
+            "callbackUrl": "https://self-hosted-demo-backend-platform.polygonid.me/api/callback?sessionId=17302",
+            "reason": "test flow",
+            "scope": [
+                {
+                    "id": 1,
+                    "circuitId": "credentialAtomicQuerySigV2",
+                    "query": {
+                        "allowedIssuers": [
+                            "*"
+                        ],
+                        "context": "https://raw.githubusercontent.com/0xPolygonID/tutorial-examples/main/credential-schema/schemas-examples/proof-of-dao-role/proof-of-dao-role.jsonld",
+                        "credentialSubject": {
+                            "role": {
+                                "$eq": 1
+                            }
+                        },
+                        "type": "ProofOfDaoRole"
+                    }
+                }
+            ]
+        },
+        "from": "did:polygonid:polygon:mumbai:2qLhNLVmoQS7pQtpMeKHDqkTcENBZUj1nkZiRNPGgV"
+    };
 
-    return res.status(200).set('Content-Type', 'application/json').send(request);
+    return res.status(200).set('Content-Type', 'application/json').send(aaa);
 }
 
 async function Callback(req,res) {
